30431bf06c9946e55f3762f27f90aa77d8e704ec
Sakshi Chauhan <chauhansakshi4m@gmail.com>
2022-02-09 10:25:15 +0530

    sound changes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector('audio[data-key="'+e.keyCode+'"]');
+    const key = document.querySelector('div[data-key="'+e.keyCode+'"]');
     if(!audio) return;
     
     key.classList.add('playing');
@@ -8,6 +13,8 @@ function playSound(e) {
     audio.play();
 }
 
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-window.addEventListener('keydown', playSound);
+document.addEventListener('keydown', playSound);
 
